edf70a3177c7b3c268dfb8461cae57518d183915
coder samuel <samuelsantos.ss95@gmail.com>
2021-08-17 21:27:50 -0300

    Connecting to MySQL database

--- src/main/java/org/academy/springbootessentials/domain/Book.java
+++ src/main/java/org/academy/springbootessentials/domain/Book.java
@@ -2,11 +2,21 @@ package org.academy.springbootessentials.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
